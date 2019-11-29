b9b43947f121bf065dcd02969f7bddb163ba2d0a
modest0w <56058578+modest0w@users.noreply.github.com>
2019-11-29 02:29:48 -0300

    E TEEEEETRA E TEEEEEEETRA

--- Server/src/Server/Connect.java
+++ Server/src/Server/Connect.java
@@ -7,34 +7,47 @@ import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+/**
+ * recebe os comandos do editor, chama os correspondentes metodos de ServerControl e envia o feedback para o cliente novamente
+ */
 public class Connect implements Runnable{
     private Client client;
     private ObjectOutputStream outputListen;
     private ObjectInputStream inputListen;
     private Socket socket;
 
-
+    /**
+     * construtor de connect, atribui as variaveis da classe os dados recebidos por parametro
+     * @param client cliente a fazer a conexao com o servidor
+     * @param socket socket da conexao
+     * @param input objectInputStream para receber os comandos do cliente
+     * @param output objectOutputStream para enviar as informacoes
+     */
     public Connect(Client client, Socket socket, ObjectInputStream input, ObjectOutputStream output) {
         this.client = client;
         this.socket = socket;
         this.inputListen = input;
         this.outputListen = output;
+        //atribui os dados recebidos por parametro as variaveis da classe connect
     }
 
-
+    /**
+     * roda a thread e fica escutando o editor, aguardando comandos para que possa realizar as chamadas
+     */
     @Override
     public void run() {
         while (true){
-            String input = null;
+            String input = null; //reseta a string
             try {
-                input = inputListen.readUTF();
+                input = inputListen.readUTF(); //le o comando do editor
             } catch (IOException e) {
                 e.printStackTrace();
             }
-            switch (input){
+            switch (input){ //analisa qual foi o comando
                 case "novo":
                     try {
                         String nome = inputListen.readUTF();
+                        //le o nome do novo arquivo que deseja-se abrir e chama a funcao de abertura em ServerControl
                         outputListen.writeInt(ServerControl.newFile(nome,client));
                         outputListen.flush();
 
@@ -44,8 +57,8 @@ public class Connect implements Runnable{
                 break;
                 case "upload":
                     try {
-                            String nome = inputListen.readUTF();
-                            if(ServerControl.verificaArquivo(nome) == -1) {
+                            String nome = inputListen.readUTF(); //le o nome do arquivo a se fazer o upload
+                            if(ServerControl.verificaArquivo(nome) == -1) { //verifica se o arquivo ja existe no servidor
                                 outputListen.writeInt(-1);
                                 outputListen.flush();
                                 break;
@@ -62,9 +75,9 @@ public class Connect implements Runnable{
                 break;
                 case "abrir":
                     try {
-                        outputListen.writeUTF(ServerControl.listaArquivos());
+                        outputListen.writeUTF(ServerControl.listaArquivos()); //envia para o editor a lista de arquivos do servidor com indices
                         outputListen.flush();
-                        outputListen.writeUTF(ServerControl.abrir(inputListen.read(),client));
+                        outputListen.writeUTF(ServerControl.abrir(inputListen.read(),client)); //abre o arquivo do indice de indice enviado pelo editor
                         outputListen.flush();
 
                     } catch (IOException e) {
@@ -73,9 +86,9 @@ public class Connect implements Runnable{
                 break;
                 case "texto":
                     try {
-                        String texto = inputListen.readUTF();
-                        int resposta = ServerControl.recebeTexto(texto,client);
-                        outputListen.writeInt(resposta);
+                        String texto = inputListen.readUTF(); //le o texto editado pelo cliente na area de texto
+                        int resposta = ServerControl.recebeTexto(texto,client); //envia o texto para ser salvo
+                        outputListen.writeInt(resposta); //envia para o editor um inteiro indicando se o processo deu certo ou se houve um erro
                         outputListen.flush();
                     } catch (IOException e) {
                         e.printStackTrace();
